10d38e5ffcee1a64abda8d1501d62add629968a3
Your Name <you@example.com>
2019-11-01 18:28:24 +0000

    added counter in while loop and fixed sensor so that vibration gathered every 0.1 seconds

--- master.py
+++ master.py
@@ -9,18 +9,23 @@ class Master:
     def __init__(self, runTime):
         self.runTime = runTime
     def run(self):
+        counter = 0
         while True:
-            print("Rain sensor Value: "+ str(rain.rainSens()))
-            print(str(BME.BMESens1()))
-            print(str(BME.BMESens2()))
-            print(str(BME.BMESens3()))
-            print("UV A: ", uvs.uvaVal())
-            print("UV B: ", uvs.uvbVal())
-            print("UV Index: ", uvs.uviVal())
+            counter += 1
             print("wind detection: ", vib.VibrationDetected())
+            print(counter)
+            if counter % 30 == 0:
+                print("Rain sensor Value: "+ str(rain.rainSens()))
+                print(str(BME.BMESens1()))
+                print(str(BME.BMESens2()))
+                print(str(BME.BMESens3()))
+                print("UV A: ", uvs.uvaVal())
+                print("UV B: ", uvs.uvbVal())
+                print("UV Index: ", uvs.uviVal())
+            
             time.sleep(self.runTime)
             
 if __name__ == "__main__":
-    MasterFile = Master(3)
+    MasterFile = Master(0.1)
     MasterFile.run()
         
